788f026c52886adbe99cc8ccfd9304b3538e9f18
Julio.S <just.julio95@gmail.com>
2022-03-14 13:19:12 -0400

    established js function and vars

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10 + 10)
+
+console.log("Our robot's name is " + playerName);
+
+console.log(playerName + " is ready for battle!");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
