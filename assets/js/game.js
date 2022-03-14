19562fcf8ea527bc20b5a3c7fc683f66a34ba356
Julio.S <just.julio95@gmail.com>
2022-03-14 14:43:28 -0400

    created a fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,43 @@
-var playerName = window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your robot's name>");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("This logs a string, good for leaving yourself a message");
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-console.log(10 + 10)
+    // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
 
-console.log("Our robot's name is " + playerName);
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
 
-console.log(playerName + " is ready for battle!");
+    //Check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+};
 
-function fight() {
-    window.alert("The fight has begun!");
-}
-
-// fight();
+fight();
